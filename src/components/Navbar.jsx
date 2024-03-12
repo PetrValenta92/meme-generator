6b9d7f48aadec226b4ace184346d37394cd6348a
Petr Valenta <petr.valenta.92@gmail.com>
2024-03-12 21:16:14 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <nav className="navbar">
             <div className="navbar__logo">
                 <img src= { trollFace } alt="Meme troll face image" />
-                <h1>Meme Generator</h1>
+                <h2>Meme Generator</h2>
             </div>
             <p>React Course - Project 3</p>
         </nav>
