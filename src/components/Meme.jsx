06ebf1eff5b20237915a85db4721492060727955
Petr Valenta <petr.valenta.92@gmail.com>
2024-03-17 22:49:33 +0100

    Add useState() to render a random meme image from memesData.js

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,7 +1,18 @@
+import data from "../memesData"
+import React from "react";
+
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("");
+
+    function getMeme() {
+        const memeArray = data.data.memes;
+        const memeIndex = Math.floor(Math.random() * memeArray.length) + 1;
+        setMemeImage(memeArray[memeIndex].url);
+    }
+
     return (
         <main>
-            <form className="form" action="" method="">
+            <div className="form" action="" method="">
                 <p>
                     <label className = "form-row" htmlFor="top">Top text</label>
                     <input className = "form-row" type="text" id="top" placeholder="Top text" />
@@ -10,8 +21,10 @@ export default function Meme() {
                     <label className = "form-row" htmlFor="bottom">Bottom text</label>
                     <input className = "form-row" type="text" id="bottom" placeholder="Bottom text"/>
                 </p>       
-                <input className = "form-row--btn" type="submit" value="Get a new meme image 🖼️" />
-            </form>
+                {/* <input className = "form-row--btn" type="submit" value="Get a new meme image 🖼️" /> */}
+                <button className = "form-row--btn" onClick={ getMeme }>Get a new meme image 🖼️</button>
+            </div>
+            <img className="meme-image" src={ memeImage } alt="Image of randome meme" />
         </main>
     )
 }
